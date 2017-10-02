f1cd5d99012bd28998deaea750b4a3effe1703e0
prakashnarkhede <prakashnarkhede89@gmail.com>
2017-10-02 21:09:49 +0530

    message

---
+++ HtmlUnitDriver/src/samplePackage/BrowserStack2.java
@@ -0,0 +1,30 @@
+package samplePackage;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserStack2 {
+
+  public static final String USERNAME = "prakashnarkhede1";
+  public static final String AUTOMATE_KEY = "ory9DpjpK4ezq7om7pAX";
+  public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+	@Test(invocationCount = 1000)
+	public void test() throws MalformedURLException{
+    DesiredCapabilities caps = new DesiredCapabilities().firefox();
+    caps.setPlatform(Platform.ANY);
+    caps.setBrowserName(BrowserType.EDGE);
+    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+    driver.get("https://helpautomation.blogspot.com/p/home.html");
+    driver.quit();
+
+  }
+}
